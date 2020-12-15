1a45253510cc77a7e4854b8c195188859bebcf6d
Unknown <max007446@gmail.com>
2020-12-15 22:06:46 +0300

    Переименовал переменные,скопировал входной слайс

--- bubble/BubbleSort.go
+++ bubble/BubbleSort.go
@@ -9,30 +9,32 @@ import (
 
 func main() {
 	inputNums := []int64{}
-	scn := bufio.NewScanner(os.Stdin)
+	scanner := bufio.NewScanner(os.Stdin)
 	fmt.Printf("Введите 999 для окончания ввода данных в слайс: \n")
-	for scn.Scan() {
-		num, er := strconv.ParseInt(scn.Text(), 10, 64)
+	for scanner.Scan() {
+		num, err := strconv.ParseInt(scanner.Text(), 10, 64)
 
 		if num == 999 {
 			break
 		}
-		if er != nil {
-			panic(er)
+		if err != nil {
+			panic(err)
 		}
 		inputNums = append(inputNums, num)
 	}
-	t := true //флаг сортировки
-	for t {
-		t = false
+	sliseSortUnSort := true //флаг сортировки
+	sortSlise := make([]int64, len(inputNums))
+	copy(sortSlise, inputNums)
+	for sliseSortUnSort {
+		sliseSortUnSort = false
 		//перебор элементов слайса
-		for j := 0; j < len(inputNums)-1; j++ {
-			if inputNums[j] > inputNums[j+1] { // сравниваем текущий элемент с предыдущим
-				inputNums[j], inputNums[j+1] = inputNums[j+1], inputNums[j] //обмениваемся элементами при выполнении условия
-				t = true
+		for j := 0; j < len(sortSlise)-1; j++ {
+			if sortSlise[j] > sortSlise[j+1] { // сравниваем текущий элемент с предыдущим
+				sortSlise[j], sortSlise[j+1] = sortSlise[j+1], sortSlise[j] //обмениваемся элементами при выполнении условия
+				sliseSortUnSort = true
 			}
 		}
 	}
-
-	fmt.Println("Отсортированный слайс: \n", inputNums)
+	fmt.Println("Входной слайс: \n", inputNums)
+	fmt.Println("Отсортированный слайс: \n", sortSlise)
 }
