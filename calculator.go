07f10eda6637ae4819d7eb38448ccd0d03ae8fe8
MaxPsk <max00744@gmail.com>
2020-12-21 21:06:02 +0300

    Добавил проверку на деление а 0,добавил паники на Scanln

--- calculator.go
+++ calculator.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"math"
 	"os"
@@ -8,12 +9,32 @@ import (
 
 var a, b, res float64
 var op string
+var (
+	divError = errors.New("Divide by zero error")
+)
 
+func intdiv(a float64, b float64) (float64, error) {
+	if b == 0 {
+		return 0, divError
+	}
+
+	return a / b, nil
+}
 func main() {
 	fmt.Print("Введите первый операнд : ")
-	fmt.Scanln(&a)
+
+	_, err := fmt.Scanln(&a)
+	if err != nil {
+		panic(err)
+	}
 	fmt.Print("Введите второй операнд : ")
+	if err != nil {
+		panic(err)
+	}
 	fmt.Scanln(&b)
+	if err != nil {
+		panic(err)
+	}
 	fmt.Print("Введите необходимую операцию на операндами : ")
 	fmt.Scanln(&op)
 	switch op {
@@ -24,7 +45,11 @@ func main() {
 	case "-":
 		res = a + b
 	case "/":
-		res = a / b
+		res, err = intdiv(a, b)
+		if err != nil {
+			fmt.Println(err)
+
+		}
 	case "~a":
 		res = math.Sqrt(a)
 	case "^":
