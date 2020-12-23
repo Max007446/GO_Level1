9e223aebbe907f459cfe48964ff98ff7762220a7
MaxPsk <max00744@gmail.com>
2020-12-23 21:04:51 +0300

    Добавил проверку на ввод строки и проверку выхода из допустимых значений

--- fibonahhi.go
+++ fibonahhi.go
@@ -1,23 +1,32 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"math"
 	"os"
+	"strconv"
 )
 
 func main() {
-	cacheMap := make(map[string]uint64)
+	cacheMap := make(map[string]int64)
 	cacheMap["next"] = 0
 	cacheMap["prev"] = 0
-	var result uint64 = 0
-	var num uint64 = 0
+	var result int64 = 0
+	var num string
+	err := errors.New("Ошибка")
 	fmt.Print("Введите номер искомого числа Фибоначчи: \n")
-	fmt.Fscan(os.Stdin, &num)
+	fmt.Fscan(os.Stdin, &num, err)
+	res, err := strconv.ParseInt(num, 10, 64)
+	if err != nil {
+		panic(err)
+	}
 
-	fmt.Println("Число Фибоначчи: \n", fibo(num, cacheMap, result))
+	fmt.Println("Число Фибоначчи: \n", fibo(res, cacheMap, result))
+
 }
 
-func fibo(n uint64, cacheMap map[string]uint64, result uint64) uint64 {
+func fibo(n int64, cacheMap map[string]int64, result int64) int64 {
 	if n == 0 {
 		return result
 	}
@@ -28,5 +37,9 @@ func fibo(n uint64, cacheMap map[string]uint64, result uint64) uint64 {
 	result = cacheMap["prev"] + cacheMap["next"]
 	cacheMap["next"] = cacheMap["prev"]
 	cacheMap["prev"] = result
+	if cacheMap["prev"] > math.MaxInt64 || cacheMap["prev"] < 0 {
+		panic("число вышло за допустимые разряды для типа int64")
+	}
 	return fibo(n-1, cacheMap, result)
+
 }
